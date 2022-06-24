b7eb84a03a4aefcd4b476e73fa9545cde31a15e2
Jan Gallardo <jmgr@outlook.cl>
2022-06-24 09:45:54 -0400

    feat: iterator

--- tserie.go
+++ tserie.go
@@ -32,3 +32,40 @@ func Normal(std, mean float64) func(time.Time) float64 {
 		return (rand.NormFloat64() * std) + mean
 	}
 }
+
+// TimeIterator generates datapoints in the same way as MakeTS but is suitable for big datasets
+type TimeIterator struct {
+	start time.Time
+	stop  time.Time
+	step  time.Duration
+	curr  Point
+	gen   func(time.Time) float64
+}
+
+// Next returns true while current time is before stop
+func (it *TimeIterator) Next() bool {
+	if !it.curr.Time.Before(it.stop) {
+		return false
+	}
+	t := it.curr.Time.Add(it.step)
+	it.curr = Point{
+		Time:  t,
+		Value: it.gen(t),
+	}
+	return true
+}
+
+//  Item returns the current data point
+func (it *TimeIterator) Item() Point {
+	return it.curr
+}
+
+// NewTimeIterator creates a new TimeIterator
+func NewTimeIterator(start, stop time.Time, step time.Duration, gen func(time.Time) float64) *TimeIterator {
+	return &TimeIterator{
+		start: start,
+		stop:  stop,
+		step:  step,
+		gen:   gen,
+	}
+}
